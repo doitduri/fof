3887792e2184e7d342d902b7433059d954836019
doitduri <kdle_@naver.com>
2021-02-07 17:39:57 +0900

    Initial Commit

---
+++ makeus/makeusApp.swift
@@ -0,0 +1,17 @@
+//
+//  makeusApp.swift
+//  makeus
+//
+//  Created by 김두리 on 2021/02/07.
+//
+
+import SwiftUI
+
+@main
+struct makeusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
